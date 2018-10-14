a981601886db1ef6a71a12dd2ca59ca701903959
barrybecker4 <barrybecker4@yahoo.com>
2018-10-14 09:10:34 -0700

    Use bb4-common version of readTextFile.

--- scala-source/com/barrybecker4/aikido/generation/AppHtmlGenerator.scala
+++ scala-source/com/barrybecker4/aikido/generation/AppHtmlGenerator.scala
@@ -1,7 +1,7 @@
 // Copyright by Barry G. Becker, 2014. Licensed under MIT License: http://www.opensource.org/licenses/MIT
 package com.barrybecker4.aikido.generation
 
-import com.barrybecker4.common.util.FileUtil
+import com.barrybecker4.common.util.FileUtil._
 import org.w3c.dom.Document
 import java.io.FileOutputStream
 import java.io.IOException
@@ -12,7 +12,7 @@ import java.io.IOException
   */
 object AppHtmlGenerator {
   /** location where data files are read from */
-  val PROJECT_DIR: String = FileUtil.getHomeDir + "scala-source/com/barrybecker4/aikido/generation/"
+  val PROJECT_DIR: String = getHomeDir + "scala-source/com/barrybecker4/aikido/generation/"
   private val JAVASCRIPT_FILE = PROJECT_DIR + "methods.js"
   private val BODY_HTML_FILE = PROJECT_DIR + "body.html"
 }
@@ -30,10 +30,10 @@ class AppHtmlGenerator() {
     fos.write(HtmlUtil.getHTMLHead("Aikido Technique Builder").getBytes)
     fos.write(HtmlUtil.getScriptOpen.getBytes)
     fos.write(new XmlToJsConverter().generateJavaScript(document).getBytes)
-    fos.write(LocalFileUtil.readTextFile(AppHtmlGenerator.JAVASCRIPT_FILE).getBytes)
+    fos.write(readTextFile(AppHtmlGenerator.JAVASCRIPT_FILE).getBytes)
     fos.write(HtmlUtil.getScriptClose.getBytes)
     fos.write("</head>\n".getBytes)
-    fos.write(LocalFileUtil.readTextFile(AppHtmlGenerator.BODY_HTML_FILE).getBytes)
+    fos.write(readTextFile(AppHtmlGenerator.BODY_HTML_FILE).getBytes)
     fos.write("</html>\n".getBytes)
     fos.close()
   }
